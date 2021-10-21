847a674058ca0b4b97e4d3a39a87cfa07f09aff3
肖明辉 <xiao.xmh@bitsun-inc.com>
2021-10-21 17:12:33 +0800

    丐版

---
+++ .history/src/index_20211021170858.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-21 17:05:08
+ * @LastEditTime: 2021-10-21 17:08:58
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \zhajinhua\src\index.ts
+ */
---
+++ .umirc.ts
@@ -0,0 +1,18 @@
+/*
+ * @Author: your name
+ * @Date: 2021-10-21 17:05:08
+ * @LastEditTime: 2021-10-21 17:08:06
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \zhajinhua\.umirc.ts
+ */
+import { defineConfig } from 'dumi';
+
+export default defineConfig({
+  title: '炸金花',
+  favicon:
+    'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
+  outputPath: 'docs-dist',
+  // more config: https://d.umijs.org/config
+});
